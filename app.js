5f9aeace0e7c373847a4a4861c68ad2df1d61c0f
Bl00dclot <bomboklad59@gmail.com>
2024-09-18 20:04:05 +0400

    Complete

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 import express from "express";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(morgan("dev"));
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
+})
+app.post("/submit", (req, res) => {
+    const ccNumber = req.body["ccNumber"];
+    const ccHolder = req.body["ccHolder"];
+    const validUntil = req.body["validUntil"];
+    const cvcNumber = req.body["cvcNumber"];
+    res.render("index.ejs", {
+        ccNumberOutput: ccNumber,
+        ccHolderOutput: ccHolder,
+        validUntilOutput: validUntil,
+        cvcNumberOutput: cvcNumber 
+    })
+})
+app.get("/about", (req, res) => {
+    res.render("about.ejs")
 })
 
 app.listen(port, () => {
